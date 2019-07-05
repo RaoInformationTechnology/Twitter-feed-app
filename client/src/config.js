467e7c51c301b4c4fcb00a040b5b18be90962887
VM8198 <malvivivek8198@gmail.com>
2019-07-05 15:52:39 +0530

    port related changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:4000/";
-const baseUrl = "http://twitter-feeds.raoinformationtechnology-conduct.tk:4000/";
+const baseUrl = "https://twitter-feeds.raoinformationtechnology-conduct.tk:4000/";
 
 export const config = {
     baseApiUrl: baseUrl
